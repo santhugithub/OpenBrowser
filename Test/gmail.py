d77db1812caf528140574a4effdcba87baaa931d
Santhosh NS <47071974+santhugithub@users.noreply.github.com>
2019-02-16 19:39:22 +0530

    Add files via upload

--- Test/gmail.py
+++ Test/gmail.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time
-driver=webdriver.Chrome(executable_path="F:/Selenium/chromedriver.exe")
+driver=webdriver.Chrome()
 driver.get("https://www.gmail.com/")
 driver.implicitly_wait(10)
 driver.find_element_by_xpath("//*[@autocomplete='username' and @class='whsOnd zHQkBf']").send_keys("santoshn.shankar@gmail.com")
@@ -9,21 +9,9 @@ driver.find_element_by_xpath("//*[@jsname='YPqjbf' and @name='password']").send_
 driver.find_element_by_xpath("//*[@class='RveJvd snByac']").click()
 driver.find_element_by_xpath("//*[@gh='cm' and @style='user-select: none;']").click()
 driver.maximize_window()
-handles = driver.window_handles
-size=len(handles)
-print(size)
-print(type(handles)) #The type will be of LIST
-parent_handle = driver.current_window_handle # current_window_handle gives you the ID of the current window
-print(parent_handle)
-for x in range(size):
-    if handles[x] != parent_handle:
-        driver.switch_to.window(handles[x])
-        print(driver.title)
-        driver.find_element_by_xpath("//*[@class='Am Al editable LW-avf' and @hidefocus='true']").send_keys("Test")
-        time.sleep(5)
-        driver.close()
-        break
-
-
-
-
+driver.find_element_by_xpath("//*[@class='aoD hl' and @tabindex='1']").click()
+driver.find_element_by_xpath("//*[@class='vO' and @aria-label='To' and @rows='1' and @autocorrect='off']").send_keys("srishti24sharma1992@gmail.com")
+driver.find_element_by_xpath("//*[@name='subjectbox' and @aria-label='Subject']").send_keys("1 4 3")
+driver.find_element_by_xpath("//*[@hidefocus='true' and @aria-label='Message Body']").send_keys("As subject says i may 'hate' or 'love' you. What do you think!!?")
+driver.find_element_by_xpath("//*[@role='button' and @data-tooltip='Send ‪(Ctrl-Enter)‬']").click()
+time.sleep(10)
